dc0ec6a94457543c028957fec998eef217b998a4
Your Name <you@example.com>
2023-12-18 18:59:33 -0500

    updated backend to have multiple endpoints

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ class DataUpdater {
 	constructor() {
 		this.apiEndpoint = 'https://services.nvd.nist.gov/rest/json/cves/2.0?noRejected&isVulnerable&cpeName=';
 		this.CPEs = ['cpe:2.3:a:gitlab:gitlab:8.5.0:*:*:*:*:*:*:*','cpe:2.3:a:github:github:3.0.0:*:*:*:*:*:*:*','cpe:2.3:a:home-assistant:home-assistant:2022.03:*:*:*:*:*:*:*','cpe:2.3:a:kubernetes:kubernetes:1.6.5:*:*:*:*:*:*:*','cpe:2.3:a:matrix:synapse:3.0.0:*:*:*:*:*:*:*'];
-		this.CPEshort = ['gitlab','github','home-assistant','kubernetes','synapse'];
+		this.CPEshort = ['gitlab','github','homeassistant','kubernetes','synapse'];
 		this.updateInterval = 12*60*60*1000;
 		this.data = {};
 		this.fetchDataAndUpdate();
@@ -16,6 +16,13 @@ class DataUpdater {
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
 	
+	limitString(str, lenLimit){
+		if(str.length > lenLimit){
+			return str.substring(0,lenLimit)+'...'
+		}
+		return str;
+	}
+
 	async fetchDataAndUpdate() {
 		try {
 			for (let index in this.CPEs) {
@@ -23,7 +30,8 @@ class DataUpdater {
 				this.data[this.CPEshort[index]] = response.data.vulnerabilities.map(obj => {
 					return {
 						id: obj.cve.id,
-						description: obj.cve.descriptions[0].value 
+						status: obj.cve.vulnStatus,
+						description: this.limitString(obj.cve.descriptions[0].value, 150)
 					};
 				});
 				await this.sleep(1000);
@@ -40,8 +48,20 @@ class APIServer {
 		this.port = port;
 		this.dataUpdater = dataUpdater;
 		this.app = express();
-		this.app.get('/data', (req, res) => {
-			res.json(this.dataUpdater.data);
+		this.app.get('/gitlab', (req, res) => {
+			res.json(this.dataUpdater.data.gitlab);
+		});
+		this.app.get('/github', (req, res) => {
+			res.json(this.dataUpdater.data.github);
+		});
+		this.app.get('/homeassistant', (req, res) => {
+			res.json(this.dataUpdater.data.homeassistant);
+		});
+		this.app.get('/kubernetes', (req, res) => {
+			res.json(this.dataUpdater.data.kubernetes);
+		});
+		this.app.get('/data/synapse', (req, res) => {
+			res.json(this.dataUpdater.data.synapse);
 		});
 	}
 	
